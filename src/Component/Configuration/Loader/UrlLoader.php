25e2d365a73cb6891bd2e9c889b547284c580f25
Guillaume Cavana <guillaume.cavana@gmail.com>
2015-12-11 13:12:34 +0100

    differentiate fonts from images

--- src/Component/Configuration/Loader/UrlLoader.php
+++ src/Component/Configuration/Loader/UrlLoader.php
@@ -29,7 +29,8 @@ final class UrlLoader implements LoaderInterface, ConfigExtensionInterface
                 ->addDefaultsIfNotSet()
                 ->children()
                     ->scalarNode('limit')->defaultValue(1000)->end()
-                    ->scalarNode('extensions')->defaultValue('png,gif,jpg,jpeg,svg,woff,woff2,eot,ttf')->end()
+                    ->scalarNode('font_extensions')->defaultValue('svg,woff,woff2,eot,ttf')->end()
+                    ->scalarNode('image_extensions')->defaultValue('png,gif,jpg,jpeg')->end()
                 ->end()
             ->end();
     }
@@ -41,13 +42,35 @@ final class UrlLoader implements LoaderInterface, ConfigExtensionInterface
             return [new CodeBlock()];
         }
 
-        $extensions = str_replace([' ', ','], ['', '|'], $this->config['extensions']);
-        $limit      = $this->config['limit'];
+        $limit            = $this->config['limit'];
+        $image_code_block = [];
+        $font_code_block  = [];
 
-        return [(new CodeBlock())->set(CodeBlock::LOADER, [sprintf(
-            '{ test: /\.(%s)$/, loader: \'url-loader?limit=%d&name=[name]-[hash].[ext]\' }',
-            $extensions,
-            $limit
-        )])];
+        if (isset($this->config['font_extensions'])) {
+            $font_extensions = explode(',', $this->config['font_extensions']);
+
+            foreach ($font_extensions as $font) {
+                $font_code_block[] = (new CodeBlock())->set(CodeBlock::LOADER, [sprintf(
+                    '{ test: /\.%s(\?v=\d+\.\d+\.\d+)?$/, loader: \'url-loader?limit=%d&name=[name]-[hash].[ext]\' }',
+                    $font,
+                    $limit
+                )]);
+            }
+        }
+
+        if (isset($this->config['image_extensions'])) {
+            $image_extensions = str_replace([' ', ','], ['', '|'], $this->config['image_extensions']);
+
+            $image_code_block = [(new CodeBlock())->set(CodeBlock::LOADER, [sprintf(
+                '{ test: /\.(%s)$/, loader: \'url-loader?limit=%d&name=[name]-[hash].[ext]\' }',
+                $image_extensions,
+                $limit
+            )])];
+        }
+
+        return array_merge(
+            $image_code_block,
+            $font_code_block
+        );
     }
 }
